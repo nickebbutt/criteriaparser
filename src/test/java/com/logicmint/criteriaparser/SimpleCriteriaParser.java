147f1a959313b5e0eb32a949327a4a53a7d71731
Nick Ebbutt <nick@logicmint.co.uk>
2021-12-17 11:23:57 +0000

    Simplify grammar

--- src/test/java/com/logicmint/criteriaparser/SimpleCriteriaParser.java
+++ src/test/java/com/logicmint/criteriaparser/SimpleCriteriaParser.java
@@ -10,69 +10,88 @@ public class SimpleCriteriaParser {
 
     public static void main(String[] args) throws IOException {
 
-        CharStream charStream = CharStreams.fromReader(new StringReader("'Nick' LIKE 'Nick'"));
+        CharStream charStream = CharStreams.fromReader(new StringReader("Nick LIKE 'Nick'"));
         CriteriaLexer lexer = new CriteriaLexer(charStream);
         CommonTokenStream tokens = new CommonTokenStream(lexer);
 
         CriteriaParser parser = new CriteriaParser(tokens);
-
-
-        CriteriaListener l = new CriteriaListener() {
-
-
-            @Override
-            public void enterExpression(CriteriaParser.ExpressionContext ctx) {
-
-            }
-
-            @Override
-            public void exitExpression(CriteriaParser.ExpressionContext ctx) {
-
-            }
-
-            @Override
-            public void enterCondition(CriteriaParser.ConditionContext ctx) {
-
-            }
-
-            @Override
-            public void exitCondition(CriteriaParser.ConditionContext ctx) {
-
-            }
-
-            @Override
-            public void enterOperand(CriteriaParser.OperandContext ctx) {
-
-            }
-
-            @Override
-            public void exitOperand(CriteriaParser.OperandContext ctx) {
-
-            }
-
-            @Override
-            public void visitTerminal(TerminalNode node) {
-                System.out.println(node.getSymbol().getText());
-            }
-
-            @Override
-            public void visitErrorNode(ErrorNode node) {
-
-            }
-
-            @Override
-            public void enterEveryRule(ParserRuleContext ctx) {
-
-            }
-
-            @Override
-            public void exitEveryRule(ParserRuleContext ctx) {
-
-            }
-        };
+        CriteriaListener l = new MyCriteriaListener();
 
         parser.addParseListener(l);
         ParseTree tree = parser.expression();
         System.out.println(tree.toStringTree(parser));
+    }
+
+    private static class MyCriteriaListener implements CriteriaListener {
+
+        @Override
+        public void visitTerminal(TerminalNode node) {
+
+        }
+
+        @Override
+        public void visitErrorNode(ErrorNode node) {
+
+        }
+
+        @Override
+        public void enterEveryRule(ParserRuleContext ctx) {
+
+        }
+
+        @Override
+        public void exitEveryRule(ParserRuleContext ctx) {
+
+        }
+
+        @Override
+        public void exitTerm(CriteriaParser.TermContext ctx) {
+
+        }
+
+        @Override
+        public void enterTerm(CriteriaParser.TermContext ctx) {
+
+        }
+
+        @Override
+        public void exitTermlist(CriteriaParser.TermlistContext ctx) {
+
+        }
+
+        @Override
+        public void enterTermlist(CriteriaParser.TermlistContext ctx) {
+
+        }
+
+        @Override
+        public void exitComparison_operator(CriteriaParser.Comparison_operatorContext ctx) {
+
+        }
+
+        @Override
+        public void enterComparison_operator(CriteriaParser.Comparison_operatorContext ctx) {
+
+        }
+
+        @Override
+        public void exitCondition(CriteriaParser.ConditionContext ctx) {
+
+        }
+
+        @Override
+        public void enterCondition(CriteriaParser.ConditionContext ctx) {
+
+        }
+
+        @Override
+        public void exitExpression(CriteriaParser.ExpressionContext ctx) {
+
+        }
+
+        @Override
+        public void enterExpression(CriteriaParser.ExpressionContext ctx) {
+
+        }
     }
 }
